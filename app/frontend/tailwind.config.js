e5267840f720bc96a03afcad5d3851eb1eb8e072
rikahei <rikaheijp@gmail.com>
2025-03-08 01:39:26 +0900

    install flowbite-vue

--- app/frontend/tailwind.config.js
+++ app/frontend/tailwind.config.js
@@ -3,10 +3,11 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
   ],
   darkMode: 'class', // Enable dark mode with class strategy
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
