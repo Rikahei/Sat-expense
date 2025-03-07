2252ca5f3b8ecc317f0becbe8bd75b01129336d7
rikahei <rikaheijp@gmail.com>
2025-03-08 00:02:14 +0900

    Add indexedDB

--- app/frontend/src/components/Home.vue
+++ app/frontend/src/components/Home.vue
@@ -4,9 +4,7 @@
   >
     <!-- Top Section: Bitcoin Price (Card Layout) -->
     <div class="flex-[3] flex flex-col items-center justify-center p-4">
-      <div
-        class="w-full max-w-md bg-white rounded-lg shadow-md p-6 dark:bg-gray-800"
-      >
+      <div class="w-full max-w-md bg-white rounded-lg shadow-md p-6 dark:bg-gray-800">
         <div class="flex items-center">
           <div class="w-3/10">
             <Icon
@@ -41,18 +39,18 @@
 
     <!-- Middle Section: Input and Button on Same Line -->
     <div class="flex-[2] flex items-center justify-center gap-4 p-4 mx-auto">
-        <input
-          type="number"
-          class="border border-gray-300 rounded-md p-2 w-64 dark:bg-gray-700 dark:text-gray-100"
-          placeholder="Enter spending amount"
-          v-model="spendingAmount"
-        />
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          @click="addSpending"
-        >
-          Add
-        </button>
+      <input
+        type="number"
+        class="border border-gray-300 rounded-md p-2 w-64 dark:bg-gray-700 dark:text-gray-100"
+        placeholder="Enter spending amount"
+        v-model="spendingAmount"
+      />
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="addSpending"
+      >
+        Add
+      </button>
     </div>
 
     <!-- Bottom Section: Spending History -->
@@ -62,42 +60,56 @@
           Spending History
         </h2>
         <p class="dark:text-gray-300">
-            Total: ${{ totalSpending }}
-            <span class="flex items-center text-xl" v-if="price">
-                 <Icon
-                    icon="bitcoin-icons:bitcoin-circle-filled"
-                    class="w-4 h-4 mr-1 text-yellow-500"
-                  />
-                {{ convertUsdToBtc(totalSpending) }}
-            </span>
-            <span v-else>Loading Total</span>
-        </p>
-      </div>
-      <ul v-if="spendingHistory.length > 0" class="w-full max-w-md mx-auto">
-        <li
-          v-for="(item, index) in spendingHistory"
-          :key="index"
-          class="flex justify-between items-center py-2"
-        >
-          <span>${{ item.amount }}</span>
-          <span class="flex items-center">
+          Total: ${{ totalSpending }}
+          <span class="flex items-center text-xl" v-if="price">
             <Icon
-              icon="bitcoin-icons:bitcoin-circle-outline"
+              icon="bitcoin-icons:bitcoin-circle-filled"
               class="w-4 h-4 mr-1 text-yellow-500"
             />
-            {{ convertUsdToBtc(item.amount) }}
+            {{ totalBtcSpending }}
           </span>
-          <span>{{ item.timestamp }}</span>
-        </li>
-      </ul>
-      <p v-else>No spending history yet.</p>
+          <span v-else>Loading Total</span>
+        </p>
+      </div>
+      <div v-if="spendingHistory.length > 0" class="w-full max-w-md mx-auto">
+        <table class="w-full text-left dark:text-gray-300">
+          <thead class="border-b border-transparent">
+            <tr>
+              <th class="py-2">Amount</th>
+              <th class="py-2">BTC Amount</th>
+              <th class="py-2">Timestamp</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(item, index) in spendingHistory"
+              :key="index"
+              class="border-b border-transparent"
+            >
+              <td class="py-2">${{ item.amount }} {{ item.currency_type }}</td>
+              <td class="py-2 flex items-center">
+                <Icon
+                  v-if="item.crypto_type === 'btc'"
+                  icon="bitcoin-icons:bitcoin-circle-outline"
+                  class="w-4 h-4 mr-1 text-yellow-500"
+                />
+                {{ item.crypto_amount }}
+              </td>
+              <td class="py-2">{{ formatTimestamp(item.timestamp) }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <p v-else class="dark:text-gray-300">No spending history yet.</p>
     </div>
   </div>
 </template>
 
 <script>
+import { DateTime } from 'luxon';
 import axios from 'axios';
 import { Icon } from '@iconify/vue';
+import { addSpendingEntry, getAllSpendingEntries } from '../utils/db';
 
 export default {
   name: 'Home',
@@ -111,7 +123,7 @@ export default {
       priceChange: null,
       previousPrice: null,
       intervalId: null,
-      spendingAmount: "",
+      spendingAmount: '',
       spendingHistory: [],
     };
   },
@@ -119,30 +131,27 @@ export default {
     totalSpending() {
       return this.spendingHistory.reduce((sum, item) => sum + item.amount, 0).toFixed(2);
     },
+    totalBtcSpending() {
+      return this.spendingHistory.reduce((sum, item) => sum + parseFloat(item.crypto_amount), 0).toFixed(8);
+    },
   },
-  mounted() {
-    this.fetchPrice(); // Initial fetch
-    this.intervalId = setInterval(this.fetchPrice, 30000); // Fetch every 30 seconds
+  async mounted() {
+    this.fetchPrice();
+    this.intervalId = setInterval(this.fetchPrice, 30000);
     this.applyDarkMode();
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", this.applyDarkMode);
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', this.applyDarkMode);
+    await this.loadSpendingHistory();
   },
   unmounted() {
-    clearInterval(this.intervalId); // Clear interval when component unmounts
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .removeEventListener("change", this.applyDarkMode);
+    clearInterval(this.intervalId);
+    window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', this.applyDarkMode);
   },
   methods: {
     applyDarkMode() {
-      if (
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches
-      ) {
-        document.documentElement.classList.add("dark");
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.classList.add('dark');
       } else {
-        document.documentElement.classList.remove("dark");
+        document.documentElement.classList.remove('dark');
       }
     },
     async fetchPrice() {
@@ -165,35 +174,45 @@ export default {
       } catch (err) {
         this.error = err.message;
         if (err.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
           this.error = `${err.message}: ${err.response.status} - ${err.response.statusText}`;
         } else if (err.request) {
-          // The request was made but no response was received
-          // `err.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
           this.error = `No response: ${err.message}`;
         } else {
-          // Something happened in setting up the request that triggered an Error
           this.error = `Error: ${err.message}`;
         }
       }
     },
-    addSpending() {
-      //Check if spendingAmount is number and not empty
-      if (this.spendingAmount && !isNaN(Number(this.spendingAmount))) {
+    async addSpending() {
+      if (this.spendingAmount && !isNaN(Number(this.spendingAmount)) && Number(this.spendingAmount) > 0) {
         const amount = Number(this.spendingAmount);
-        this.spendingHistory.push({
+        const currentTimestamp = new Date(); 
+        const cryptoAmount = Number(amount / this.price).toFixed(8);
+        const entry = {
+          currency_type: 'usd',
           amount: amount,
-          timestamp: new Date().toLocaleString(),
-        });
-        this.spendingAmount = "";
+          crypto_type: 'btc',
+          crypto_amount: cryptoAmount,
+          crypto_price: this.price,
+          timestamp: currentTimestamp, 
+        };
+        await addSpendingEntry(entry);
+        this.spendingHistory.push(entry);
+        this.spendingAmount = '';
+      } else {
+        alert('Please enter a valid amount greater than zero.');
       }
     },
-    convertUsdToBtc(usdAmount) {
-        if (!this.price || isNaN(Number(usdAmount))) return "Invalid";
-        const btcAmount = Number(usdAmount) / Number(this.price);
-        return btcAmount.toFixed(8);
+    formatTimestamp(timestamp) {
+      const dt = DateTime.fromJSDate(timestamp);
+      return dt.toFormat('yy/MM/dd, HH:mm');
+    },
+    async loadSpendingHistory() {
+      try {
+        const entries = await getAllSpendingEntries();
+        this.spendingHistory = entries;
+      } catch (error) {
+        console.error('Error loading spending history:', error);
+      }
     },
   },
 };
---
+++ app/frontend/src/utils/db.js
@@ -0,0 +1,34 @@
+// src/utils/db.js
+import { openDB } from 'idb';
+
+const DB_NAME = 'spending-db';
+const DB_VERSION = 1;
+const SPENDING_STORE = 'spending';
+
+async function initDatabase() {
+  return openDB(DB_NAME, DB_VERSION, {
+    upgrade(db, oldVersion, newVersion) {
+      if (oldVersion < 1) {
+        db.createObjectStore(SPENDING_STORE, {
+          autoIncrement: true, // Automatically generate unique keys
+        });
+      }
+    },
+  });
+}
+
+async function addSpendingEntry(entry) {
+  const db = await initDatabase();
+  const tx = db.transaction(SPENDING_STORE, 'readwrite');
+  const store = tx.objectStore(SPENDING_STORE);
+  await store.add(entry); // Add the entry
+  await tx.done;
+  console.log('Spending entry added:', entry);
+}
+
+async function getAllSpendingEntries() {
+    const db = await initDatabase();
+    return db.getAll(SPENDING_STORE);
+}
+
+export { addSpendingEntry, getAllSpendingEntries };
